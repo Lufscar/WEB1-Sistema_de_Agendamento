4629bf06edf3cdd1111d591c3225ef72abd550cb
= <=>
2021-10-29 15:07:52 -0300

    correcao

--- SistemaAgendamento/src/main/java/br/ufscar/dc/dsw/controller/HomeController.java
+++ SistemaAgendamento/src/main/java/br/ufscar/dc/dsw/controller/HomeController.java
@@ -1,13 +1,18 @@
 package br.ufscar.dc.dsw.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.ufscar.dc.dsw.domain.Pessoa;
 import br.ufscar.dc.dsw.security.PessoaDetails;
@@ -32,9 +37,9 @@ public class HomeController {
 		return "home";
 	}
 	
-	@GetMapping("/homecontroller")
-	public String listarhome(@RequestParam(value="area", required = false) String area, ModelMap model) {
-		model.addAttribute("profissionais", service.buscarPorArea(area));
+	@PostMapping("/homecontroller")
+	public String listarhome(@Valid String area, RedirectAttributes attr) {
+		attr.addAttribute("profissionais", service.buscarPorArea(area));
 		return "home";
 	}
 }
